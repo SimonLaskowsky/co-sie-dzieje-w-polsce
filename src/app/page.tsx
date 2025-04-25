b60894ff0c9561f5706c0e1d10ab43d8759fdfd0
Szymon Laskowski <simonlaskowsky@gmail.com>
2025-04-25 13:34:28 +0200

    feat: implement search functionality in SearchBar and CardGrid components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+"use client"
 
 import SearchBar from "@/components/shared/SearchBar";
 import CardGrid from "@/components/shared/CardGrid";
+import { useState } from "react";
 
 const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
     <div className="flex flex-col gap-20 items-center justify-items-center w-full min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <SearchBar />
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <main className="w-full h-full">
-        <CardGrid />
+        <CardGrid searchQuery={searchQuery} />
       </main>
     </div>
   );
