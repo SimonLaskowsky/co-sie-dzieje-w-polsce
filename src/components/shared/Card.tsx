63f4f375e910f6705f6ac6a2308c44d2d7aac8d0
Szymon Laskowski <simonlaskowsky@gmail.com>
2025-04-25 14:24:17 +0200

    fix: update heading styles in Card component for improved readability

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -19,7 +19,7 @@ const Card = ({
         isImportant ? "border-red-500/50" : "border-neutral-200 dark:border-neutral-700"
       } flex flex-col gap-2 p-5 rounded-3xl shadow-md cursor-pointer hover:ring-2 
       dark:hover:ring-neutral-100 hover:ring-neutral-300 hover:border-transparent transition-all duration-300 h-fit`}>
-        <h3 className="text-lg font-semibold tracking-tight line-clamp-3">{title}</h3>
+        <h3 className="text-lg leading-snug font-semibold tracking-tight line-clamp-3">{title}</h3>
         {categories.length > 0 && (
             <div className="flex flex-wrap gap-1.5">
             {categories.map((category, index) => (
