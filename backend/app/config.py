483dd3df1c854b8c7c9c77386784c18c74ca5f5a
Rurek5000 <kagorski99@gmail.com>
2025-05-08 09:18:17 +0200

    feat: update config

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 load_dotenv()
 
+BASIC_URL = os.getenv("BASIC_URL")
 API_URL = os.getenv("DU_URL")
+VOTING_URL = os.getenv("VOTING_URL")
 CURRENT_YEAR = datetime.now().year
 LAST_KNOWN_FILE = Path("last_known.json")
