02edf727241e51509b780845dbb98620cb6c1e1e
Rurek5000 <kagorski99@gmail.com>
2025-05-08 09:18:08 +0200

    feat: created fetch_one_law api call

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,39 @@
 import requests
-from config import API_URL, CURRENT_YEAR
+from config import BASIC_URL, API_URL, CURRENT_YEAR, VOTING_URL
+
+def fetch_one_law(eli):
+    if not BASIC_URL:
+        print("Error: DU_URL is not set in .env file")
+        return None
+        
+    url = f"{BASIC_URL}//{eli}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print("API error:", response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Connection error:", e)
+        return None
+
+def get_voting_data(url):
+    if not url:
+        print("Error: url is not set")
+        return None
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print("API error:", response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Connection error:", e)
+        return None
 
 def fetch_api_data():
     if not API_URL:
