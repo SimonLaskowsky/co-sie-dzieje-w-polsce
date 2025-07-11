fdecbd5c071a0d2be9364efceb23b2ef4824eed9
Szymon <simonlaskowsky@gmail.com>
2025-07-11 14:05:20 +0200

    fix: refactor className formatting for improved readability in Card component

--- frontend/src/components/shared/Card.tsx
+++ frontend/src/components/shared/Card.tsx
@@ -58,10 +58,9 @@ const Card = ({
   return (
     <div
       onClick={onClick}
-      className={`bg-neutral-700/10 dark:bg-neutral-800/40 mx-auto max-w-11/12 sm:max-w-80 rounded-3xl ${
-        isImportant ? 'border-2 border-red-500/70 shadow-red-500/10' : ''
-      } flex flex-col gap-3 p-5 rounded-3xl shadow-xl cursor-pointer hover:ring-2 
-      dark:hover:ring-neutral-100 hover:ring-neutral-300 hover:!border-transparent transition-all duration-300 h-fit`}
+      className={`bg-neutral-700/10 dark:bg-neutral-800/40 mx-auto max-w-11/12 sm:max-w-80 flex flex-col gap-3 p-5 rounded-3xl shadow-xl cursor-pointer hover:ring-2 
+      dark:hover:ring-neutral-100 hover:ring-neutral-300 hover:!border-transparent transition-all duration-300 h-fit
+      ${isImportant && 'border-2 border-red-500/70 shadow-red-500/10'}`}
     >
       <div className="dark:text-neutral-600 text-neutral-500 text-xs">
         {formattedDate}
