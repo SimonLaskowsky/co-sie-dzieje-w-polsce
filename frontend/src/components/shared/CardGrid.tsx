c4fead0c4b10ada238dd32f51585486682d4145a
Szymon <simonlaskowsky@gmail.com>
2025-07-27 15:21:57 +0200

    fix: update console log to display acts instead of data in CardGrid component

--- frontend/src/components/shared/CardGrid.tsx
+++ frontend/src/components/shared/CardGrid.tsx
@@ -24,7 +24,8 @@ const CardGrid = ({ searchQuery, selectedTypes, data }: CardGridProps) => {
 
   const { acts } = data || {};
 
-  console.log(data);
+  console.log(acts);
+
   const breakpointColumnsObj = {
     default: 4,
     1200: 3,
